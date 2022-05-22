f9a9c2afe8b869a7af8df732d6105bef9978ac39
Morgan <morgan.nott@hotmail.com>
2022-05-22 21:47:37 +0200

    .toUpperCase() Pipe implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
-import {Controller, Get, Param} from '@nestjs/common';
+import {Controller, Get, Param, UsePipes} from '@nestjs/common';
 import { AppService } from './app.service';
+import {UpperPipe} from "./upper.pipe";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get(':name')
+  @UsePipes(UpperPipe)
   getHello(@Param('name') name: string ): string {
     return this.appService.getHello(name);
   }
