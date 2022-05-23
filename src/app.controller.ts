19150dc020cb7b4e3307f57b2308e8866f3de2cf
Morgan <morgan.nott@hotmail.com>
2022-05-23 14:33:30 +0200

    parseInt() Pipe Implementation on GET request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Get, Param, Post, UsePipes} from '@nestjs/common';
 import { AppService } from './app.service';
 import {UpperPipe} from "./pipes/upper.pipe";
 import {UpperPipeOnObject} from "./pipes/upper-pipe-on-object";
+import {ParseIntPipe} from "./pipes/parse-int.pipe";
 
 class Message {
 }
@@ -15,6 +16,15 @@ export class AppController {
   getHello(@Param('name') name: string ): string {
     return this.appService.getHello(name);
   }
+  @Get('articles/:id')
+  @UsePipes(ParseIntPipe)
+  getArticleById(@Param('id') id)  {
+    const idType = typeof id;
+    const res = { id: id, idType: idType};
+    console.log(res);
+    return res;
+  }
+
   @Post()
   @UsePipes(UpperPipeOnObject)
   createMessage(@Body() message: Message) {
